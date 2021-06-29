2ddd6a103d18b6a2bf06bc160001e02ad96f482f
Rirush <RaccoonRirush@gmail.com>
2021-06-29 12:18:07 +0800

    feat: add comments

--- main.cu
+++ main.cu
@@ -1,22 +1,28 @@
 #include <cstdio>
 #include <cinttypes>
 
-#define BLOCK_SIZE 13
+#define BLOCK_SIZE 25
 
-__global__ void akf(uint64_t start_offset, uint8_t *output) {
-    __shared__ uint8_t akf[BLOCK_SIZE];
+// Задача 1. Определить бит для каждой нити с учетом того, что каждой нити прихоидтся N (около 3-5, больше 5 может отсутствовать какой-либо выигрыш) чисел
+// Задача 2. Расчитать АКФ для каждого числа
+// Задача 3. Запись результата в глобальную память одна на блок, наилучший результат
+//
+// Можно использовать атомики чтобы писать в глобальную память только одно значение
+// Начальный пик всегда ярко выражен, поэтому его учитывать не надо
+// Париться о длинной арифметике не надо - больше 32-33 бит посчитать не получится в адекватное время
+
+__global__ void akf(uint64_t start_offset, int8_t *output) {
+    __shared__ int8_t akf[BLOCK_SIZE];
+    __shared__ uint8_t bits[BLOCK_SIZE];
     size_t idx = threadIdx.x; // offset
     uint64_t val = blockIdx.x + start_offset; // signal
 
-    uint8_t bits[BLOCK_SIZE];
-    for(uint8_t i = 0; i < BLOCK_SIZE; i++) {
-        bits[BLOCK_SIZE - i - 1] = val & (1);
-        val >>= 1;
-    }
+    bits[BLOCK_SIZE - idx - 1] = (val >> idx) & 1;
+    __syncthreads();
 
-    uint8_t out = 0;
+    int8_t out = 0;
     for(size_t i = 0; i < BLOCK_SIZE - idx; i++) {
-        out += bits[i + idx] & bits[i];
+        out -= (bits[i + idx] ^ bits[i]) * 2 - 1;
     }
 
     akf[idx] = out;
@@ -30,10 +36,10 @@ __global__ void akf(uint64_t start_offset, uint8_t *output) {
 }
 
 int main() {
-    uint8_t *dev_output;
+    int8_t *dev_output;
     cudaMalloc((void**)&dev_output, BLOCK_SIZE);
-    auto output = new uint8_t[BLOCK_SIZE];
-    akf<<<1, BLOCK_SIZE>>>(0b1111100110101, dev_output);
+    auto output = new int8_t[BLOCK_SIZE];
+    akf<<<1, BLOCK_SIZE>>>(0b1110011100000010101001001, dev_output);
     cudaMemcpy(output, dev_output, BLOCK_SIZE, cudaMemcpyDeviceToHost);
     for(size_t i = 0; i < BLOCK_SIZE; i++) {
         printf("%d ", output[i]);
