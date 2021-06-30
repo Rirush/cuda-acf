eb24ee94e194a80928f0829656e6d41f05834569
Rirush <RaccoonRirush@gmail.com>
2021-06-30 10:59:36 +0800

    feat: read numbers of bits from args

--- main.cu
+++ main.cu
@@ -2,7 +2,7 @@
 #include <cinttypes>
 #include <chrono>
 
-#define BLOCK_SIZE 35
+#define BLOCK_SIZE 64
 
 // Задача 1. Определить бит для каждой нити с учетом того, что каждой нити прихоидтся N (около 3-5, больше 5 может отсутствовать какой-либо выигрыш) чисел
 // Задача 2. Расчитать АКФ для каждого числа
@@ -12,7 +12,7 @@
 // Начальный пик всегда ярко выражен, поэтому его учитывать не надо
 // Париться о длинной арифметике не надо - больше 32-33 бит посчитать не получится в адекватное время
 
-__global__ void akf(uint64_t start_offset, uint64_t end, uint64_t *min_amplitude, uint64_t *signal, uint64_t *counter) {
+__global__ void akf(uint64_t start_offset, uint64_t end, uint64_t *min_amplitude, uint64_t *signal, uint64_t n) {
     __shared__ int8_t akf[BLOCK_SIZE];
     __shared__ uint8_t bits[BLOCK_SIZE];
     __shared__ uint64_t amplitude;
@@ -20,11 +20,11 @@ __global__ void akf(uint64_t start_offset, uint64_t end, uint64_t *min_amplitude
     uint64_t val = blockIdx.x + start_offset; // signal
     while (val <= end) {
         amplitude = 0;
-        bits[BLOCK_SIZE - idx - 1] = (val >> idx) & 1;
+        bits[n - idx - 1] = (val >> idx) & 1;
         __syncthreads();
 
         int8_t out = 0;
-        for (size_t i = 0; i < BLOCK_SIZE - idx; i++) {
+        for (size_t i = 0; i < n - idx; i++) {
             out -= (bits[i + idx] ^ bits[i]) * 2 - 1;
         }
 
@@ -62,23 +62,33 @@ void cpu_akf(uint64_t value, uint8_t b, int8_t *akf) {
     delete[] bits;
 }
 
-int main() {
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        printf("Usage: analysis [size]\n");
+        return EXIT_FAILURE;
+    }
+    int n = atoi(argv[1]);
+    printf("Using %d bits\n", n);
+    if(n >= 64) {
+        printf("Cannot process more than 63 bits\n");
+        return EXIT_FAILURE;
+    }
+    if(n < 5) {
+        printf("Cannot process less than 5 bits\n");
+        return EXIT_FAILURE;
+    }
     std::chrono::time_point<std::chrono::steady_clock> start_time = std::chrono::high_resolution_clock::now();
     uint64_t *dev_amplitude;
     uint64_t *dev_signal;
-    uint64_t *dev_counter;
     cudaMalloc((void**)&dev_amplitude, sizeof(uint64_t));
     cudaMalloc((void**)&dev_signal, sizeof(uint64_t));
-    cudaMalloc((void**)&dev_counter, sizeof(uint64_t));
     cudaMemset(dev_amplitude, 0xFF, sizeof(uint64_t));
-    cudaMemset(dev_counter, 0x00, sizeof(uint64_t));
     uint64_t amplitude;
     uint64_t signal;
-    uint64_t counter;
 
-    uint64_t blocks = (1ULL << BLOCK_SIZE) - 1ULL;
-    uint64_t start = 0b1ULL << (BLOCK_SIZE - 1);
-    akf<<<2048, BLOCK_SIZE>>>(start, blocks, dev_amplitude, dev_signal, dev_counter);
+    uint64_t blocks = (1ULL << n) - 1ULL;
+    uint64_t start = 0b1ULL << (n - 1);
+    akf<<<2048, n>>>(start, blocks, dev_amplitude, dev_signal, n);
     cudaError_t cudaError = cudaGetLastError();
     if(cudaError != cudaSuccess) {
         printf("Could not execute CUDA kernel: %s\n", cudaGetErrorString(cudaError));
@@ -86,18 +96,14 @@ int main() {
     }
     cudaMemcpy(&amplitude, dev_amplitude, sizeof(uint64_t), cudaMemcpyDeviceToHost);
     cudaMemcpy(&signal, dev_signal, sizeof(uint64_t), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&counter, dev_counter, sizeof(uint64_t), cudaMemcpyDeviceToHost);
     char buf[66];
-    char mask[66];
     _ui64toa(signal, buf, 2);
-    _ui64toa(counter, mask, 2);
     printf("Best signal is %s (%llu) with amplitude of %llu\n", buf, signal, amplitude);
-    printf("Processed mask: %s\n", mask);
 
-    auto *akf = new int8_t[BLOCK_SIZE]();
-    cpu_akf(signal, BLOCK_SIZE, akf);
+    auto *akf = new int8_t[n]();
+    cpu_akf(signal, n, akf);
     printf("AKF is ");
-    for(int8_t i = 0; i < BLOCK_SIZE; i++) {
+    for(int8_t i = 0; i < n; i++) {
         printf("%d ", akf[i]);
     }
     printf("\n");
