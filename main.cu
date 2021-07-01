fbd34984dc9daa1b9775f6f11148d95b822dba3e
Rirush <RaccoonRirush@gmail.com>
2021-07-01 11:20:24 +0800

    fix: add more comments

--- main.cu
+++ main.cu
@@ -17,24 +17,28 @@ __global__ void akf(uint64_t start_offset, uint64_t end, uint64_t *min_amplitude
     __shared__ int8_t akf[BLOCK_SIZE];
     __shared__ uint8_t bits[BLOCK_SIZE];
     __shared__ uint64_t amplitude;
-    size_t idx = threadIdx.x; // offset
-    uint64_t val = blockIdx.x + start_offset; // signal
+    size_t idx = threadIdx.x; // Сдвиг по сигналу
+    uint64_t val = blockIdx.x + start_offset; // Сам сигнал
     while (val <= end) {
+        // Сброс амплитуды и разбитие сигналы на биты
         amplitude = 0;
         bits[n - idx - 1] = (val >> idx) & 1;
         __syncthreads();
 
+        // Подсчет АКФ для каждого сдвига
         int8_t out = 0;
         for (size_t i = 0; i < n - idx; i++) {
             out -= (bits[i + idx] ^ bits[i]) * 2 - 1;
         }
 
+        // Взятие АКФ по модулю и вычисление амплитуды бокового лепестка
         akf[idx] = (out >= 0) ? out : 0 - out;
         if (idx != 0) {
             atomicMax(reinterpret_cast<unsigned long long int*>(&amplitude), (unsigned long long)akf[idx]);
         }
         __syncthreads();
 
+        // Проверка амплитуды с глобальной памятью и запись лучшего сигнала
         if (idx == 0) {
             uint64_t old = atomicMin(reinterpret_cast<unsigned long long int*>(min_amplitude), (unsigned long long)amplitude);
             if (old >= amplitude) {
@@ -44,7 +48,7 @@ __global__ void akf(uint64_t start_offset, uint64_t end, uint64_t *min_amplitude
 
         val += gridDim.x;
         __syncthreads();
-    };
+    }
 }
 
 void cpu_akf(uint64_t value, uint8_t b, int8_t *akf) {
@@ -64,10 +68,12 @@ void cpu_akf(uint64_t value, uint8_t b, int8_t *akf) {
 }
 
 int main(int argc, char **argv) {
+    // Проверяем был ли передан аргумент с количеством бит
     if(argc != 2) {
         printf("Usage: analysis [size]\n");
         return EXIT_FAILURE;
     }
+    // Конвертируем аргумент в число и проверяем можем ли мы такое число бит перебрать
     int n = atoi(argv[1]);
     printf("Using %d bits\n", n);
     if(n >= 64) {
@@ -78,6 +84,7 @@ int main(int argc, char **argv) {
         printf("Cannot process less than 5 bits\n");
         return EXIT_FAILURE;
     }
+    // Создаем переменные для замера времени и работы CUDA
     auto start_time = std::chrono::high_resolution_clock::now();
     uint64_t *dev_amplitude;
     uint64_t *dev_signal;
@@ -87,6 +94,7 @@ int main(int argc, char **argv) {
     uint64_t amplitude;
     uint64_t signal;
 
+    // Инициализируем ядро и передаем данные на графический процессор
     uint64_t blocks = (1ULL << n) - 1ULL;
     uint64_t start = 0b1ULL << (n - 1);
     akf<<<3072, n>>>(start, blocks, dev_amplitude, dev_signal, n);
@@ -95,11 +103,14 @@ int main(int argc, char **argv) {
         printf("Could not execute CUDA kernel: %s\n", cudaGetErrorString(cudaError));
         return EXIT_FAILURE;
     }
+
+    // Забираем данные с графического процессора и выводим итоговый сигнал с амплитудой
     cudaMemcpy(&amplitude, dev_amplitude, sizeof(uint64_t), cudaMemcpyDeviceToHost);
     cudaMemcpy(&signal, dev_signal, sizeof(uint64_t), cudaMemcpyDeviceToHost);
     std::bitset<BLOCK_SIZE> s(signal);
     printf("Best signal is %s (%llu) with amplitude of %llu\n", s.to_string().c_str(), signal, amplitude);
 
+    // Пересчитываем АКФ на процессоре чтобы вывести его
     auto *akf = new int8_t[n]();
     cpu_akf(signal, n, akf);
     printf("AKF is ");
@@ -107,7 +118,9 @@ int main(int argc, char **argv) {
         printf("%d ", akf[i]);
     }
     printf("\n");
+
+    // Выводим количество времени которое у нас ушло на перебор значений
     auto end_time = std::chrono::high_resolution_clock::now();
-    printf("Calculation took %f seconds", (double)(std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count()) / 1e+6);
+    printf("Calculation took %f seconds\n", (double)(std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count()) / 1e+6);
     return 0;
 }
